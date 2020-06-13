3a01a9f942f9f6a521497390be37df9ff67cf9a6
Nikolay Zapolnov <zapolnov@gmail.com>
2020-06-13 12:33:07 +0300

    After lesson.

--- Assets/Scripts/NGetText/NGetTextDemo.cs
+++ Assets/Scripts/NGetText/NGetTextDemo.cs
@@ -7,9 +7,12 @@ using NGettext;
 
 public class NGetTextDemo : MonoBehaviour
 {
+    public int lifeCount = 1;
+
     void Awake()
     {
         var text = GetComponent<TextMeshProUGUI>();
-        text.text = T._("Hello, world!");
+        //text.text = T._("Hello, world!");
+        text.text = T._n("You have {0} life.", "You have {0} lifes.", lifeCount, lifeCount);
     }
 }
