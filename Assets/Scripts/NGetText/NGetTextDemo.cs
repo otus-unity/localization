8c485e86b66b705ea3995143239c87e0e2b92de6
Nikolay Zapolnov <zapolnov@gmail.com>
2020-10-22 21:39:10 +0300

    After lesson.

--- Assets/Scripts/NGetText/NGetTextDemo.cs
+++ Assets/Scripts/NGetText/NGetTextDemo.cs
@@ -7,9 +7,13 @@ using NGettext;
 
 public class NGetTextDemo : MonoBehaviour
 {
+    public int lifeCount = 1;
+
     void Awake()
     {
         var text = GetComponent<TextMeshProUGUI>();
-        text.text = T._("Hello, world!");
+        //text.text = T._("Hello, world!");
+        //text.text = T._n("You have a life.", "You have many lifes.", lifeCount);
+        text.text = T._n("You have {0} life.", "You have {0} lifes.", lifeCount, lifeCount);
     }
 }
--- Assets/Scripts/TryParse/TryParseDemo.cs
+++ Assets/Scripts/TryParse/TryParseDemo.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.Globalization;
 using UnityEngine;
 
 public class TryParseDemo : MonoBehaviour
 {
     void Awake()
     {
-        float value = float.Parse("1.23");
-        string str = value.ToString();
+        float value = float.Parse("1.23", CultureInfo.InvariantCulture);
+        string str = value.ToString(CultureInfo.InvariantCulture);
         Debug.Log(str);
-        Debug.Log($"{value}");
+        Debug.Log(FormattableString.Invariant($"{value}"));
     }
 }
